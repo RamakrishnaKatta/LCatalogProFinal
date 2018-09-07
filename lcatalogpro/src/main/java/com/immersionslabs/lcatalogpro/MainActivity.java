fbfa0f84227296fb67cb451138e77c6dc83a58ef
Naganath <nagnath.m@immersionslabs.com>
2018-09-07 14:19:10 +0530

    Augment activity added

--- lcatalogpro/src/main/java/com/immersionslabs/lcatalogpro/MainActivity.java
+++ lcatalogpro/src/main/java/com/immersionslabs/lcatalogpro/MainActivity.java
@@ -1,13 +1,26 @@
 package com.immersionslabs.lcatalogpro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton Augmentbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Augmentbutton=findViewById(R.id.button_augment);
+
+        Augmentbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AugmentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
